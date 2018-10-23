24dc0257d806ae6d5f6b8c1824b93838e64ffaee
Adam Surgenor <adam@surgenor.me>
2018-10-23 20:47:36 +0100

    Minor refactor of Basket

--- src/ShoppingBasket/Basket.cs
+++ src/ShoppingBasket/Basket.cs
@@ -1,20 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
 
 namespace ShoppingBasket
 {
     public class Basket
     {
         public decimal NetTotal() => Items().Sum(item => item.Product.Price * item.Quantity);
-        public ICollection<BasketItem> Items() => BasketItemsByName.Values;
+        public ICollection<BasketItem> Items() => _basketItemsByName.Values;
         
-        private IDictionary<string, BasketItem> BasketItemsByName = new Dictionary<string, BasketItem>();
+        private readonly IDictionary<string, BasketItem> _basketItemsByName = new Dictionary<string, BasketItem>();
 
         public void AddItem(Product product)
         {
-            BasketItemsByName.Add(product.Name, new BasketItem(product,1));
+            _basketItemsByName.Add(product.Name, new BasketItem(product,1));
         }
     }
 }
