c45495df5104568ff19f9e3b07fa7afacb8b9852
Adam Surgenor <adam@surgenor.me>
2018-10-23 20:47:11 +0100

    Finish testing one item

--- test/ShoppingBasket.Test/BasketTests.cs
+++ test/ShoppingBasket.Test/BasketTests.cs
@@ -14,13 +14,41 @@ namespace ShoppingBasket.Test
         }
 
         [Fact]
-        public void WhenAProductIsAddedThenTheBasketContainsTheCorrectItems()
+        public void WhenAProductIsAddedThenTheNetTotalShouldBeCorrect()
         {
             var basket = new Basket();
 
             basket.AddItem(new Product("Dove Soap", 39.99m));
             
             basket.NetTotal().Should().Be(39.99m);
+        }
+
+        [Fact]
+        public void BasketItemsShouldBeImmutable()
+        {
+            var basket = new Basket();
+
+            basket.AddItem(new Product("Dove Soap", 39.99m));
+            
+            basket.Items().IsReadOnly.Should().BeTrue();
+        }
+
+        [Fact]
+        public void WhenAProductIsAddedThenTheBasketContainsTheCorrectItems()
+        {
+            var basket = new Basket();
+
+            basket.AddItem(new Product("Dove Soap", 39.99m));
+
+            basket.Items().Should().BeEquivalentTo(
+                new[]
+                {
+                    new
+                    {
+                        Product = new {Name = "Dove Soap"},
+                        Quantity = 1
+                    }
+                });
         }
     }
 }
